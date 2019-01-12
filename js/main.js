c83c0a2b2fd2251389ed337584897d66c40b3584
Nikita Bolude <44002016+Nikita1999UA@users.noreply.github.com>
2019-01-12 19:12:11 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
+$(function() {
+    $('.slider_main').slick({
+        dots: true,
+        infinite: true,
+        arrows: false,
+        speed: 2500,
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+    $('.popup-modal').magnificPopup({
+        type: 'inline',
+        preloader: false,
+        focus: '#username',
+        modal: true
+    });
+    $(document).on('click', '.popup-modal-dismiss', function(e) {
+        e.preventDefault();
+        $.magnificPopup.close();
+    });
 
-$('.slider_main').slick({
-  dots: true,
-  infinite: true,
-     arrows: false,
-  speed: 2500,
-  slidesToShow: 1,
-     autoplay: true,
-  autoplaySpeed: 2000,
-})
+    $('.menu-wrapper').on('click', function() {
+		$('.hamburger-menu').toggleClass('animate');
+		$('.aside-nav').toggleClass('active');
+	})
+});
